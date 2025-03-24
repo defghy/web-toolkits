3be95603c740e4424b0e734fd18740f452f9c19a
yu.hu <yu.hu@happyelements.com>
2025-03-24 10:30:11 +0800

    update 0.0.4

--- packages/wtool-chrome-bridge/src/index.ts
+++ packages/wtool-chrome-bridge/src/index.ts
@@ -3,9 +3,10 @@ import { ContentBridge } from './contexts/content'
 import { DevtoolBridge } from './contexts/devtool'
 import { BackgroundBridge } from './contexts/background'
 import { IFrame, IFrameTop } from './contexts/iframe'
-import { Plat } from './const'
+import { BaseBridge } from './base'
+import { Plat, MsgDef } from './const'
 
 export const IFrameBridge = IFrame
 export const IFrameTopBridge = IFrameTop
 
-export { WebBridge, ContentBridge, DevtoolBridge, BackgroundBridge, Plat, IFrame, IFrameTop }
+export { BaseBridge, WebBridge, ContentBridge, DevtoolBridge, BackgroundBridge, Plat, MsgDef, IFrame, IFrameTop }
