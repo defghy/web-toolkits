e601390999ccd680be79c2cd502aba41393fb936
yu.hu <yu.hu@happyelements.com>
2026-01-11 13:44:22 +0800

    cloneDeepWith第一版本

---
+++ packages/wtool-common/README.md
@@ -0,0 +1,6 @@
+# web tools
+
+- cloneDeepWith
+  - similar with `lodash`'s `cloneDeepWith`
+  - support recursive `customizer` return object
+  - support `Symbol('delete')`，can prune object
---
+++ packages/wtool-common/scripts/publish.mjs
@@ -0,0 +1,44 @@
+import { promises as fsp } from 'fs'
+import path from 'path'
+import { $ } from 'execa'
+
+import packageJson from '../package.json' with { type: 'json' };
+
+// 修改package.json
+const changePackage = function (newJson) {
+  const packageJsonPath = path.resolve('./package.json')
+  const content = JSON.stringify(newJson, null, 2)
+  return fsp.writeFile(packageJsonPath, content)
+}
+
+const run = async function () {
+  // build
+  const { stdout } = await $`npm run build`
+  console.log(stdout)
+
+  // prepublish
+  const newJson = JSON.parse(JSON.stringify(packageJson))
+
+  Object.assign(newJson, {
+    name: '@yuhufe/web-common',
+    private: false,
+  })
+  Object.keys(newJson.dependencies).forEach(key => {
+    if (key.startsWith('@aweb')) {
+      Reflect.deleteProperty(newJson.dependencies, key)
+    }
+  })
+  await changePackage(newJson)
+
+  // publishing
+  try {
+    const { stdout } = await $`npm publish`
+    console.log(stdout)
+  } catch (e) {
+    console.error(e)
+  }
+
+  // postpublish
+  await changePackage(packageJson)
+}
+run()
