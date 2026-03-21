cb71157c51f691268d205a5111956806ab5d35db
huyu <huyu_911@126.com>
2026-03-21 20:26:04 +0800

    tmp save

---
+++ packages/wtool-diff/src/DiffViewer.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import type * as Monaco from 'monaco-editor'
+  import MonacoEditor from './MonacoEditor.svelte'
+
+  type DiffEditorOptions = Monaco.editor.IStandaloneDiffEditorConstructionOptions
+  type ModelOptions = Monaco.editor.ITextModelUpdateOptions
+
+  let {
+    originalCode = '',
+    modifiedCode = '',
+    language = 'plaintext',
+    options = {},
+    modelOptions = {},
+    width = '100%',
+    height = '400px',
+  }: {
+    originalCode?: string
+    modifiedCode?: string
+    language?: string
+    options?: DiffEditorOptions
+    modelOptions?: ModelOptions
+    width?: string
+    height?: string
+  } = $props()
+</script>
+
+<div class="diff-view-wrap">
+  <MonacoEditor
+    {originalCode}
+    {modifiedCode}
+    {language}
+    {options}
+    {modelOptions}
+    {width}
+    {height}
+  />
+</div>
+
+<style>
+  .diff-view-wrap {
+    min-height: 200px;
+  }
+</style>
