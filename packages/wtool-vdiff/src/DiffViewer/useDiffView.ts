e6a94d8e2ba763f29e92f4292fc139c8ada4b7a5
yu.hu <yu.hu@happyelements.com>
2026-03-29 12:59:36 +0800

    api设定

--- packages/wtool-vdiff/src/DiffViewer/useDiffView.ts
+++ packages/wtool-vdiff/src/DiffViewer/useDiffView.ts
@@ -3,8 +3,8 @@ import { useCompExp } from '@yuhufe/web-ui'
 // 存放单个diffEditor的数据
 export const useDiffViewer = function ({ isMaster = false }) {
   const exp = useCompExp<{
-    changeViewed
-    changeCollaspe
+    updateViewed: (viewed: boolean) => any // 控制是否viewed
+    updateUnchangedCollpase: (args: boolean) => any // 控制是否收起展开
   }>({ isMaster, key: 'diffViewer' })
 
   return { ...exp }
