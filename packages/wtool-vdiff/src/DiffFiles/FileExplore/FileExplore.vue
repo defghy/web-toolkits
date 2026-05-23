b6140da1ddf7ec3bc09b47850dcc45d8763e1337
huyu <huyu_911@126.com>
2026-05-23 17:14:37 +0800

    文件架子

---
+++ packages/wtool-vdiff/src/DiffFiles/FileExplore/FileExplore.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="file-explore-wrap"></div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, nextTick } from 'vue'
+
+import { loadingDirective } from '@yuhufe/web-ui'
+import type { DiffEditorOptions, WtoolDiffViewerProps, ModelOptions } from '@/types'
+
+const props = withDefaults(
+  defineProps<{
+    diffFiles: WtoolDiffViewerProps[]
+  }>(),
+  {
+    diffFiles: () => [],
+  }
+)
+</script>
+
+<style scoped></style>
