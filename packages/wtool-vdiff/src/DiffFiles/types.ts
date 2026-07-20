55c2a7ed3649f48a3b600002c0741b3f4c326ba0
yu.hu <yu.hu@happyelements.com>
2026-07-20 19:43:20 +0800

    必要类型

--- packages/wtool-vdiff/src/DiffFiles/types.ts
+++ packages/wtool-vdiff/src/DiffFiles/types.ts
@@ -2,3 +2,9 @@ import type { FileTree } from '../types'
 
 // 文件列表的每个文件类型
 export type FileItem = Omit<FileTree, 'isDirectory' | 'children'>
+
+export interface DiffFileState {
+  height: number
+  viewed: boolean
+  isRaw: boolean
+}
